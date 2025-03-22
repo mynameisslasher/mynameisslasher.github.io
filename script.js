11fe440e19d34c9496b7b24b5e3b39ab689e48a3
Robert <robertp@MacBook-R.local>
2025-03-22 13:20:32 +0500

    Adding style.css, fixing missed map

--- script.js
+++ script.js
@@ -1,78 +1,82 @@
 let map, userPlacemark, followUser = true, lastCoords = null, deviceAngle = 0;
 
 function init() {
-    map = new ymaps.Map("map", {
-        center: [56.8519, 60.6122],
-        zoom: 17,
-        controls: [],
-        behaviors: ["drag", "dblClickZoom", "multiTouch", "scrollZoom", "rotate"]
-    });
+  map = new ymaps.Map("map", {
+    center: [56.8519, 60.6122],
+    zoom: 17,
+    controls: [],
+    behaviors: ["drag", "scrollZoom", "multiTouch", "dblClickZoom"]
+  });
 
-    navigator.geolocation.watchPosition(updateUserLocation, error => console.error("Ошибка геолокации:", error), {
-        enableHighAccuracy: true
-    });
+  // Слежение за движением пользователя
+  navigator.geolocation.watchPosition(updateUserLocation, 
+    err => console.error("Ошибка геолокации:", err), 
+    { enableHighAccuracy: true });
 
-    window.addEventListener("deviceorientation", event => {
-        deviceAngle = event.alpha || 0;
-    });
+  // Получение угла устройства (не во всех браузерах)
+  window.addEventListener("deviceorientationabsolute", event => {
+    if (event.alpha !== null) {
+      deviceAngle = event.alpha;
+    }
+  }, true);
 
-    map.events.add("actionbegin", () => followUser = false);
+  map.events.add("actionbegin", () => followUser = false);
 
-    fetch("camera_coordinates.json")
-        .then(response => response.json())
-        .then(cameras => {
-            cameras.forEach(camera => {
-                let coords = camera.geometry.coordinates;
-                let props = camera.properties;
-                let placemark = new ymaps.Placemark(coords, {
-                    hintContent: props.hintContent,
-                    balloonContentBody: props.balloonContentBody
-                }, {
-                    preset: "islands#redIcon"
-                });
-                map.geoObjects.add(placemark);
-            });
-        })
-        .catch(error => console.error("Ошибка загрузки камер:", error));
+  // Загрузка камер
+  fetch("camera_coordinates.json")
+    .then(res => res.json())
+    .then(cameras => {
+      cameras.forEach(camera => {
+        const coords = camera.geometry.coordinates;
+        const props = camera.properties;
+        const placemark = new ymaps.Placemark(coords, {
+          hintContent: props.hintContent,
+          balloonContentBody: props.balloonContentBody
+        }, {
+          preset: "islands#redIcon"
+        });
+        map.geoObjects.add(placemark);
+      });
+    })
+    .catch(err => console.error("Ошибка загрузки камер:", err));
 }
 
 function updateUserLocation(position) {
-    console.log("Получены координаты:", position.coords.latitude, position.coords.longitude);
-    const { latitude, longitude, heading } = position.coords;
-    const newCoords = [latitude, longitude];
+  const { latitude, longitude, heading } = position.coords;
+  const coords = [latitude, longitude];
 
-    if (!userPlacemark) {
-        userPlacemark = new ymaps.Placemark(newCoords, {}, {
-            iconLayout: "default#image",
-            iconImageHref: "arrow.png",
-            iconImageSize: [40, 40],
-            iconImageOffset: [-20, -20]
-        });
-        map.geoObjects.add(userPlacemark);
-    }
+  if (!userPlacemark) {
+    userPlacemark = new ymaps.Placemark(coords, {}, {
+      iconLayout: "default#image",
+      iconImageHref: "arrow.png",
+      iconImageSize: [40, 40],
+      iconImageOffset: [-20, -20],
+      iconRotate: true
+    });
+    map.geoObjects.add(userPlacemark);
+  }
 
-    userPlacemark.geometry.setCoordinates(newCoords);
+  userPlacemark.geometry.setCoordinates(coords);
 
-    let rotationAngle = heading !== undefined ? heading : (lastCoords ? getRotationAngle(lastCoords, newCoords) : deviceAngle);
-    userPlacemark.options.set("iconRotate", rotationAngle);
+  let angle = heading ?? (lastCoords ? getRotationAngle(lastCoords, coords) : deviceAngle);
+  userPlacemark.options.set("iconAngle", angle);
 
-    lastCoords = newCoords;
+  lastCoords = coords;
 
-    if (followUser) {
-        map.setCenter(newCoords, undefined, { duration: 300 });
-        map.setRotation(rotationAngle);
-    }
+  if (followUser) {
+    map.setCenter(coords, undefined, { duration: 300 });
+  }
 }
 
 function getRotationAngle(oldCoords, newCoords) {
-    const dx = newCoords[1] - oldCoords[1];
-    const dy = newCoords[0] - oldCoords[0];
-    return (Math.atan2(dy, dx) * 180) / Math.PI + 90;
+  const dx = newCoords[1] - oldCoords[1];
+  const dy = newCoords[0] - oldCoords[0];
+  return (Math.atan2(dy, dx) * 180) / Math.PI + 90;
 }
 
 function recenterMap() {
-    followUser = true;
-    if (userPlacemark) {
-        map.setCenter(userPlacemark.geometry.getCoordinates(), undefined, { duration: 300 });
-    }
+  followUser = true;
+  if (userPlacemark) {
+    map.setCenter(userPlacemark.geometry.getCoordinates(), undefined, { duration: 300 });
+  }
 }
